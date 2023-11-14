6855e9ce1c49f69745c294360cc635c19b797dc9
Nicolas Ramy <nicolas.ramy@darkelda.com>
2023-11-14 11:55:54 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="git-glow",
-    version="0.2.11",
+    version="0.2.12",
     packages=find_packages(),
     author="Nicolas RAMY",
     author_email="nicolas.ramy@darkelda.com",
